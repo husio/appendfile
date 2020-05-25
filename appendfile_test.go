331b87d09594d93a390f1e8044d45550d91f2104
Piotr Husiatyński <husio@users.noreply.github.com>
2020-05-25 21:47:25 +0200

    more cases

--- appendfile_test.go
+++ appendfile_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
 	"testing"
 	"time"
 )
@@ -35,6 +36,48 @@ func BenchmarkAppendSync(b *testing.B) {
 		}
 		if err := fd.Sync(); err != nil {
 			b.Fatalf("sync: %s", err)
+		}
+	}
+	b.StopTimer()
+}
+
+func BenchmarkAppendFdatasync(b *testing.B) {
+	fd, err := os.OpenFile(tempfile(b), os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := fd.Write([]byte("1234567890qwertyuiop")); err != nil {
+			b.Fatalf("write: %s", err)
+		}
+
+		if err := syscall.Fdatasync(int(fd.Fd())); err != nil {
+			b.Fatalf("fdatasync: %s", err)
+		}
+	}
+	b.StopTimer()
+}
+
+func BenchmarkAppendFdatasyncAndFallocate(b *testing.B) {
+	fd, err := os.OpenFile(tempfile(b), os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	if err := syscall.Fallocate(int(fd.Fd()), 0, 0, 1e7); err != nil {
+		b.Fatalf("fallocate: %s", err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if _, err := fd.Write([]byte("1234567890qwertyuiop")); err != nil {
+			b.Fatalf("write: %s", err)
+		}
+
+		if err := syscall.Fdatasync(int(fd.Fd())); err != nil {
+			b.Fatalf("fdatasync: %s", err)
 		}
 	}
 	b.StopTimer()
